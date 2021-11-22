9426e1fcb44f35a0b0df5ce17065eb0699cc1523
SheilaPS <sheilladasilva@hotmail.com>
2021-11-22 19:56:00 -0300

    Validações

--- app/models/livrarium.rb
+++ app/models/livrarium.rb
@@ -1,6 +1,9 @@
 class Livrarium < ApplicationRecord
     with_options presence: true do
-        validates :Titulo,:Autor,:Páginas,:Editora,:Avaliação
+        validates :Titulo,:Autor,:Páginas,:Editora
+        validates :Avaliação, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }
     end
+    
+
 
 end
