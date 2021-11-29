d4206b3839fdd2745cda0f932db29d91e8c22efb
SheilaPS <sheilladasilva@hotmail.com>
2021-11-29 19:19:20 -0300

    Ajuste Rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :livraria
+  root to: 'livraria#index'
+  resources :livraria 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
